c3ad0c7891eeaa1dcdc7256df33e7228cbbd54f1
nana808-git <nana.devtestacct@gmail.com>
2021-07-11 21:14:32 -0500

    add subnet

--- Prod/module.tf.tf
+++ Prod/module.tf.tf
@@ -2,4 +2,5 @@ module "vpc" {
   source = "../resources"
 
   cidr = "10.20.0.0/20"
+  subnet_cidr = "10.20.0.0/24"
 }
--- Stg/module.tf.tf
+++ Stg/module.tf.tf
@@ -2,4 +2,5 @@ module "vpc" {
   source = "../resources"
 
   cidr = "10.30.0.0/20"
+  subnet_cidr = "10.30.0.0/24"
 }
